c62a5e1a0fb87901e6e14a97a97dc261923e9a66
Cherik <Pourcheriki@gmail.com>
2023-04-09 11:37:57 +0330

    move tests to describe

--- src/components/Input.test.tsx
+++ src/components/Input.test.tsx
@@ -4,28 +4,34 @@ import { neutralColors, semanticColors } from '@giveth/ui-design-system';
 import Input from '@/components/Input';
 import '@testing-library/jest-dom';
 
-test('the input is rendering', () => {
-	render(<Input />);
-	const input = screen.getByRole('textbox');
-	expect(input).toBeInTheDocument();
-	expect(input).toHaveValue('');
-});
+describe('Input component', () => {
+	test('the input is rendering', () => {
+		render(<Input />);
+		const input = screen.getByRole('textbox');
+		expect(input).toBeInTheDocument();
+		expect(input).toHaveValue('');
+	});
 
-test('the input is accessible', async () => {
-	render(<Input label='test' />);
-	const input = screen.getByRole('textbox');
-	const accessibilityResults = await axe(input);
-	expect(accessibilityResults).toHaveNoViolations();
-});
+	test('the input is accessible', async () => {
+		render(<Input label='test' />);
+		const input = screen.getByRole('textbox');
+		const accessibilityResults = await axe(input);
+		expect(accessibilityResults).toHaveNoViolations();
+	});
 
-test('the input border is gray on normal', async () => {
-	render(<Input label='test' />);
-	const input = screen.getByTestId('styled-input');
-	expect(input).toHaveStyle(`border: 2px solid ${neutralColors.gray[300]}`);
-});
+	test('the input border is gray on normal', async () => {
+		render(<Input label='test' />);
+		const input = screen.getByTestId('styled-input');
+		expect(input).toHaveStyle(
+			`border: 2px solid ${neutralColors.gray[300]}`,
+		);
+	});
 
-test('the input border is red on error', async () => {
-	render(<Input label='test' error={{ message: 'Error' }} />);
-	const input = screen.getByTestId('styled-input');
-	expect(input).toHaveStyle(`border: 2px solid ${semanticColors.punch[500]}`);
+	test('the input border is red on error', async () => {
+		render(<Input label='test' error={{ message: 'Error' }} />);
+		const input = screen.getByTestId('styled-input');
+		expect(input).toHaveStyle(
+			`border: 2px solid ${semanticColors.punch[500]}`,
+		);
+	});
 });
